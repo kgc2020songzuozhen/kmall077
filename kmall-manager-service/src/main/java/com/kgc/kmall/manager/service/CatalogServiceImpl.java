281c08f7e509e84bccbf1bbeebe2d2e548fbf0be
songzuozhen_glb <songzuozhen_glb@kgc.com>
2020-12-16 16:46:52 +0800

    实现跨域，一级分类，二级分类

--- kmall-manager-service/src/main/java/com/kgc/kmall/manager/service/CatalogServiceImpl.java
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manager/service/CatalogServiceImpl.java
@@ -1,7 +1,10 @@
 package com.kgc.kmall.manager.service;
 
 import com.kgc.kmall.bean.PmsBaseCatalog1;
+import com.kgc.kmall.bean.PmsBaseCatalog2;
+import com.kgc.kmall.bean.PmsBaseCatalog2Example;
 import com.kgc.kmall.manager.mapper.PmsBaseCatalog1Mapper;
+import com.kgc.kmall.manager.mapper.PmsBaseCatalog2Mapper;
 import com.kgc.kmall.service.CatalogService;
 import org.apache.dubbo.config.annotation.Service;
 import org.springframework.stereotype.Component;
@@ -16,9 +19,20 @@ public class CatalogServiceImpl implements CatalogService{
     @Resource
     PmsBaseCatalog1Mapper pmsBaseCatalog1Mapper;
 
+    @Resource
+    PmsBaseCatalog2Mapper pmsBaseCatalog2Mapper;
+
+
     @Override
     public List<PmsBaseCatalog1> getCatalog1() {
         List<PmsBaseCatalog1> pmsBaseCatalog1s = pmsBaseCatalog1Mapper.selectByExample(null);
         return pmsBaseCatalog1s;
+    }
+
+    @Override
+    public List<PmsBaseCatalog2> getCatalog2(Integer catalog1Id) {
+        PmsBaseCatalog2Example example=new PmsBaseCatalog2Example();
+        example.createCriteria().andCatalog1IdEqualTo(catalog1Id);
+        return pmsBaseCatalog2Mapper.selectByExample(example);
     }
 }
