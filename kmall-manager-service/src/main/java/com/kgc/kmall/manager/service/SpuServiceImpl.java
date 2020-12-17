088bab89ca42ea41e8732076cce8c8a8ec222c46
songzuozhen_glb <songzuozhen_glb@kgc.com>
2020-12-17 16:12:24 +0800

    实现spu列表显示

---
+++ kmall-manager-service/src/main/java/com/kgc/kmall/manager/service/SpuServiceImpl.java
@@ -0,0 +1,28 @@
+package com.kgc.kmall.manager.service;
+
+import com.kgc.kmall.bean.PmsProductInfo;
+import com.kgc.kmall.bean.PmsProductInfoExample;
+import com.kgc.kmall.manager.mapper.PmsProductInfoMapper;
+import com.kgc.kmall.service.SpuService;
+import org.apache.dubbo.config.annotation.Service;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Component
+@Service
+public class SpuServiceImpl implements SpuService {
+
+    @Resource
+    PmsProductInfoMapper pmsProductInfoMapper;
+
+    @Override
+    public List<PmsProductInfo> spuList(Long catalog3Id) {
+        PmsProductInfoExample example=new PmsProductInfoExample();
+        PmsProductInfoExample.Criteria criteria = example.createCriteria();
+        criteria.andCatalog3IdEqualTo(catalog3Id);
+        List<PmsProductInfo> infoList = pmsProductInfoMapper.selectByExample(example);
+        return infoList;
+    }
+}
---
+++ kmallapi/src/main/java/com/kgc/kmall/service/SpuService.java
@@ -0,0 +1,9 @@
+package com.kgc.kmall.service;
+
+import com.kgc.kmall.bean.PmsProductInfo;
+
+import java.util.List;
+
+public interface SpuService {
+    List<PmsProductInfo> spuList(Long catalog3Id);
+}
