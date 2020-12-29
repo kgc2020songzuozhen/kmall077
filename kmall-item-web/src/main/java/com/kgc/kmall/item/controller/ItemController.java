6f207a742997e005a61ac4f02027b5ff8bd7efc4
songzuozhen_glb <songzuozhen_glb@kgc.com>
2020-12-29 14:18:56 +0800

    导入静态资源

--- kmall-item-web/src/main/java/com/kgc/kmall/item/controller/ItemController.java
+++ kmall-item-web/src/main/java/com/kgc/kmall/item/controller/ItemController.java
@@ -1,14 +1,14 @@
 package com.kgc.kmall.item.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ItemController {
-    @RequestMapping("/test")
-    @ResponseBody
-    public String test(){
-        return "index";
+    @RequestMapping("{skuId}.html")
+    public String item(@PathVariable(value = "skuId") String skuId){
+        return "item";
     }
 }
